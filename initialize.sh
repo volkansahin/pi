a6aa61fbf2be4ceba982aef50a02a11a5a786de8
Volkan Şahin <bm.volkansahin@gmail.com>
2017-01-28 13:17:01 +0200

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -3,12 +3,21 @@ sudo apt-get install tightvncserver
 sudo apt-get install xrdp
 
 sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
-
 network={
     ssid="The_ESSID_from_earlier"
     psk="Your_wifi_password"
 }
 
+
+/etc/network/interfaces
+
+allow-hotplug wlan0
+auto wlan0
+ 
+ 
+iface wlan0 inet dhcp
+        wpa-ssid "ssid"
+        wpa-psk "password"
 #wifi driver
 
 
